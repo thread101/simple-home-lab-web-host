4ddbfc4f03c3a8dcc1fc459604afc1d5984848bf
Alpinine99 <andreprojects001@gmail.com>
2026-01-11 14:01:29 +0300

    clean up and error handling

--- NAS-server/NAS-server.py
+++ NAS-server/NAS-server.py
@@ -1,44 +1,155 @@
 import time
-import os
-import socket
+import http.server
+import socketserver
 import threading
-import bin.server_class as sc
-import bin.shell as shell
+import os
+import sys
+import random
 
-NAME = socket.gethostname()
-IP = socket.gethostbyname(NAME)
+
+class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            if self.path == "/":
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write(self.generate_html().encode())
+
+            elif self.path == "/style.css":
+                self.send_response(200)
+                self.send_header("Content-type", "text/css")
+                self.end_headers()
+                with open("style.css", "rb") as f:
+                    self.wfile.write(f.read())
+
+            else:
+                super().do_GET()
+
+        except Exception as e:
+            print(f"\033[33;1mWarning: \033[0m{e}")
+
+    def list_directory(self, path):
+        try:
+            html = self.generate_html(path)
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(html.encode())
+
+        except OSError:
+            self.send_error(404, "File not found")
+
+    def generate_html(self, path=None):
+        html = ""
+        path = self.directory if path is None else path
+
+        _files, folders = [], []
+        for i in os.listdir(path):
+            item = os.path.join(path, i)
+            if os.path.isfile(item):
+                _files.append(i)
+
+            else:
+                folders.append(i)
+
+        _files.sort()
+        folders.sort()
+        _files.extend(folders)
+
+        for file in _files:
+            if os.path.isfile(os.path.join(path, file)):
+                html += f"""
+                    <li class="file">
+                        <a href="{file}">{file}</a>
+                        <a class="download" href="{file}" download="download-{file}">download</a> 
+                    </li>
+                """
+
+            else:
+                html += f"""
+                    <li class="directory">
+                        <a href="{file}">{file}/</a>
+                    </li>
+                """
+
+        with open("Web/index.html", "r") as f:
+            index_html = f.read()
+
+        with open("Web/style.css", "r") as f:
+            styles = f.read()
+
+        index_html = index_html.format(f"<style>{styles}</style>", path, html)
+
+        return index_html
+
+
+class server(threading.Thread):
+    def __init__(self, *args, path, port, **kwags):
+        self.path = path
+        self.port = port
+        self.instance = None
+        assert os.path.exists(path), "path doesn't exist"
+        assert PORT in range(0, 65536), "Port is out of range"
+        super().__init__(*args, **kwags)
+
+    def shutdown(self):
+        if self.instance is not None:
+            self.instance.shutdown()
+
+    def run(self):
+        path = self.path
+        port = self.port
+
+
+        class handler(CustomHTTPRequestHandler):
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, directory=path, **kwargs)
+
+        with socketserver.TCPServer(("", port), handler) as httpd:
+            self.instance = httpd
+            httpd.serve_forever()
+
+
+IP = "localhost"
+PATH = os.getcwd()
+PORT = random.randint(49152, 65535)
+TIME = 15 * 60
 
 try:
-    out = shell.run_command("ifconfig")
-    t = out[out.index("wlan0"):]
-    if "inet " in t:
-        IP = t[t.index("inet ")+5: t.index("netmask")-1]
-        IP = IP[:IP.index(" ")] if " " in IP else IP
-    
-    else:
-        print(f'no internet connection switching to local ip:{IP}\n')
+    PATH = sys.argv[1]
+    try:
+        PORT = int(sys.argv[2])
+        try: TIME = float(sys.argv[3]) * 60
+        except IndexError: pass
 
-except Exception as e:
-    print(f'failed to fetch ip\nError: {e}')
-    IP = input('input machines ip: ')
+    except IndexError:
+        pass
 
-PORT = int(input("Server on port (49152-65535)? "))
-TIME = int(input("Server runtime in minute? "))
-path = input("Complete path? ")
+except IndexError:
+    print(f"\033[33;1mYou can also pass arguments:\033[0m python {sys.argv[0]} [path] [port] [runtime(minutes)]")
 
-assert PORT in range(49152, 65535), "port selected is not a dynamic port use(49152-65535)"
-assert os.path.exists(path), "invalid path"
+finally:
+    startTime = time.time()
 
-startTime = time.time()//60
+    try:
+        localServer = server(port=PORT, path=PATH)
 
-localServer = sc.server(port=PORT, path=path)
-localServer.start()
+    except AssertionError as e:
+        print(f"\033\a[31;1mError: \033[0m{e}")
+        os._exit(1)
 
-link = f"http://{IP}:{PORT}"
-print(f"server running at: {link}\n")
+    localServer.start()
+    link = f"http://{IP}:{PORT}"
+    print(f"\033[32;1mserver running at: \033[34m{link}\033[33m ,for {TIME/60} minutes\033[0m.")
 
-while True:
-    
-    if time.time()//60 - startTime >= TIME:
-        print("\nclosing server, time elpse reached")
-        os._exit(0)
+    try:
+        while True:
+            if time.time() - startTime >= TIME:
+                print("\033[33;1mclosing server, time elapse reached\033[0m")
+                localServer.shutdown()
+                break
+
+    except KeyboardInterrupt:
+        localServer.shutdown()
+        print("\033[33;1mclosing sever on command\033[0m")
